8a3d2bb33119c55a84482783274a4976e9591bdc
sumanlc <sumlam@alu.edu.gva.es>
2024-05-07 20:48:23 +0200

    test3(holacaracola)

--- src/PasswordVerifyTest.java
+++ src/PasswordVerifyTest.java
@@ -32,6 +32,14 @@ class PasswordVerifyTest {
 
     @Test
     public void test3(){
+        String password = "\"holacaracola";
+        PasswordVerify verifier = new PasswordVerify();
+        ValidationResult result = verifier.verifyPassword(password);
+
+        assertFalse(result.isValid());
+        assertEquals("La contrasenya ha de contenir almenys 2 números\n" +
+                "La contrasenya ha de contenir almenys una lletra majúscula\n" +
+                "La contrasenya ha de contenir almenys un caràcter especial\n", result.getErrorMessage());
 
     }
 
